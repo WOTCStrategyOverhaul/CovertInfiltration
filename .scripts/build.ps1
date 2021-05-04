23075c08d3afb3a43c49dab76be4586c8057f5d6
Xymanek <xymanek@outlook.com>
2021-05-04 17:56:49 +0300

    More work on the common build

--- .scripts/build.ps1
+++ .scripts/build.ps1
@@ -10,42 +10,34 @@ $common = Join-Path -Path $ScriptDirectory "X2ModBuildCommon\build_common.ps1"
 Write-Host "Sourcing $common"
 . ($common)
 
-try {
-    $builder = [BuildProject]::new("CovertInfiltration", $srcDirectory, $sdkPath, $gamePath)
+$builder = [BuildProject]::new("CovertInfiltration", $srcDirectory, $sdkPath, $gamePath)
 
-    switch ($config)
-    {
-        "debug" {
-            $builder.EnableDebug()
-        }
-        "default" {
-            # Nothing special
-        }
-        "" { ThrowFailure "Missing build configuration" }
-        default { ThrowFailure "Unknown build configuration $config" }
+switch ($config)
+{
+    "debug" {
+        $builder.EnableDebug()
     }
-
-    # TODO: This dumps the companion package into the SDK's Src
-    $builder.IncludeSrc("$srcDirectory\X2WOTCCommunityHighlander\X2WOTCCommunityHighlander\Src")
-    $builder.IncludeSrc("$srcDirectory\X2WOTCCommunityHighlander\Components\DLC2CommunityHighlander\DLC2CommunityHighlander\Src")
-
-    $builder.IncludeSrc("$srcDirectory\SquadSelectAnyTime\SquadSelectAtAnyTime\Src")
-    $builder.AddToClean("SquadSelectAtAnyTime")
-    
-    $builder.SetContentOptionsJsonPath("$srcDirectory/ContentOptions.json")
-
-    $builder.AddPreMakeHook({
-        Write-Host "Updating version and commit..."
-        & "$srcDirectory\X2WOTCCommunityHighlander\.scripts\update_version.ps1" -ps "$srcDirectory\X2WOTCCommunityHighlander\VERSION.ps1" -srcDirectory "$sdkPath\Development\Src\" -use_commit
-
-        Write-Host "Updated."
-    })
-
-    $builder.InvokeBuild()
-   
-    # we made it!
-    SuccessMessage "*** SUCCESS! ***" "CovertInfiltration"
-} catch {
-    FailureMessage $_
-    exit
+    "default" {
+        # Nothing special
+    }
+    "" { ThrowFailure "Missing build configuration" }
+    default { ThrowFailure "Unknown build configuration $config" }
 }
+
+# TODO: This dumps the companion package into the SDK's Src
+$builder.IncludeSrc("$srcDirectory\X2WOTCCommunityHighlander\X2WOTCCommunityHighlander\Src")
+$builder.IncludeSrc("$srcDirectory\X2WOTCCommunityHighlander\Components\DLC2CommunityHighlander\DLC2CommunityHighlander\Src")
+
+$builder.IncludeSrc("$srcDirectory\SquadSelectAnyTime\SquadSelectAtAnyTime\Src")
+$builder.AddToClean("SquadSelectAtAnyTime")
+
+$builder.SetContentOptionsJsonPath("$srcDirectory/ContentOptions.json")
+
+$builder.AddPreMakeHook({
+    Write-Host "Updating version and commit..."
+    & "$srcDirectory\X2WOTCCommunityHighlander\.scripts\update_version.ps1" -ps "$srcDirectory\X2WOTCCommunityHighlander\VERSION.ps1" -srcDirectory "$sdkPath\Development\Src\" -use_commit
+
+    Write-Host "Updated."
+})
+
+$builder.InvokeBuild()
